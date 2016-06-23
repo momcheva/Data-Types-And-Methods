cd96cc4bae93d4104925fe4caa9a878210753444
momcheva <nora.momcheva@gmail.com>
2016-06-23 11:13:24 +0300

    Add files
    add homework tasks

---
+++ P05.BooleanVariable/BoolVar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P05.BooleanVariable
+{
+    class BoolVar
+    {
+        static void Main(string[] args)
+        {
+            
+                string value = Console.ReadLine();
+
+                bool isTrue = Convert.ToBoolean(value);
+
+                if (isTrue)
+                {
+                    Console.WriteLine("Yes");
+                }
+                else
+                {
+                    Console.WriteLine("No");
+                }
+            
+        }
+    }
+}
